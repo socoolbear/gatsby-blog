463531b2048837b0e9719d75467712b81f5435a2
Bong-yeon Kim <socoolbear@op.gg>
2020-07-14 16:33:03 +0900

    add study sample

--- pages/study/lotto-generator.js
+++ pages/study/lotto-generator.js
@@ -1,5 +1,48 @@
 import _ from 'underscore';
 import React from "react";
+import styled from "styled-components";
+
+const Row = styled.div`
+    display: flex;
+    flex-direction: row;
+`;
+
+const Number = styled.span`
+    display: flex;
+    width: 100px;
+    height: 100px;
+    border-radius : 50px;
+    justify-content: center;
+    align-items: center;
+    font-size: 3rem;
+    background: ${props => {
+    const n = parseInt(props.children, 10);
+
+    if (n <= 10) {
+        return 'yellow;';
+    } else if (n <= 20) {
+        return 'blue;';
+    } else if (n <= 30) {
+        return 'red;';
+    } else if (n <= 40) {
+
+        return 'black;';
+    } else {
+        return 'green;';
+    }
+}};
+    color: ${props => {
+    const n = parseInt(props.children, 10);
+    if (10 < n && n <= 20) {
+        return 'white;';
+    } else if (30 < n && n <= 40) {
+        return 'white;';
+    } else {
+        return 'black;';
+    }
+
+}};
+`;
 
 function generate() {
     let numbers = [];
@@ -21,11 +64,11 @@ const LottoGenerator = () => {
 
     return (
         <div>
-            <div className={"hero"}>
+            <Row>
                 {list.map((element, index) =>
-                        <span key={index}>{element + ' '}</span>
-                    )}
-            </div>
+                    <Number key={index}>{element}</Number>
+                )}
+            </Row>
             <div>
                 <button onClick={regenerate}>재생성</button>
             </div>
